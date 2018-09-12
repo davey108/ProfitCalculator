de45c66b56b05b3d8f76f5308f3388eb5e7610a9
dave108 <32458148+davey108@users.noreply.github.com>
2018-09-12 01:45:03 -0400

    Add files via upload

--- src/UnboundedKnapsack2.java
+++ src/UnboundedKnapsack2.java
@@ -14,7 +14,7 @@ public class UnboundedKnapsack2 {
 	protected int globalCount = 0;
     protected Item2 [] items;
     protected int n; // the number of items
-    protected Item2 sack = new Item2("sack", 0, 8500);
+    protected Item2 sack = new Item2("sack", 0, 15000);
     protected Item2  best = new Item2("best", 0, 0);
     protected int  []  maxIt;  // maximum number of items
     protected int  []    iIt;  // current indexes of items
@@ -22,45 +22,21 @@ public class UnboundedKnapsack2 {
     
     // the main() function:
     public static void main(String[] args) throws InvalidFormatException, IOException {
-    	String fpath = "C:/Users/David1234/Desktop/Nostale Vendetta Mall List";
+    	String fpath = "C:/Users/DavidPC/Desktop/Nostale Vendetta Mall List";
     	String ext = ".xlsx";
+    	System.out.println(Runtime.getRuntime().availableProcessors());
     	OPCPackage pkg = OPCPackage.open(new File(fpath+ext));
 	    XSSFWorkbook wb = new XSSFWorkbook(pkg);
-	    XSSFSheet sheet = wb.getSheetAt(0);
 	    // exclude first row
-	    int len = sheet.getPhysicalNumberOfRows()-1;
-        UnboundedKnapsack2 k1 = new UnboundedKnapsack2(fpath + ext,1,len/6);
-        UnboundedKnapsack2 k2 = new UnboundedKnapsack2(fpath + ext,len/6+1, len/6*2);
-        UnboundedKnapsack2 k3 = new UnboundedKnapsack2(fpath + ext,len/6*2+1, len/6*3);
-        UnboundedKnapsack2 k4 = new UnboundedKnapsack2(fpath + ext,len/6*3+1, len/6*4);
-        UnboundedKnapsack2 k5 = new UnboundedKnapsack2(fpath + ext,len/6*4+1, len/6*5);
-        UnboundedKnapsack2 k6 = new UnboundedKnapsack2(fpath + ext,len/6*5+1, len);
-        UnboundedKnapsack2 [] knapsacks = {k1,k2,k3,k4,k5,k6};
-        int threads_num = 6;
-        // spin up threads_num threads
-        Thread [] threads = new Thread[threads_num];
-        for(int i = 0; i < threads_num; i++){
-        	int num = i;
-        	threads[i] = new Thread(() -> {
-        		knapsacks[num].start();
-        	});
-        	threads[i].start();        		
-        }
-        // rejoin
-        for(int i = 0; i < threads_num; i++){
-        	try {
-				threads[i].join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-        }
-        System.out.println("Exitting");
+	   UnboundedKnapsack2 sack = new UnboundedKnapsack2(fpath+ext);
+	   sack.start();
+        
         wb.close();
     } // main()
  
-    public UnboundedKnapsack2(String file, int from, int to) {
+    public UnboundedKnapsack2(String file) {
     	try {
-			setUp(file,from,to);
+			setUp(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -97,19 +73,20 @@ public class UnboundedKnapsack2 {
     public void calcWithRecursion(int item) {    	
         for (int i = 0; i <= maxIt[item]; i++) {
             iIt[item] = i;
-            if(globalCount == 200) return;
+            if(globalCount == 900) return;
             if (item < n-1) {
                 calcWithRecursion(item+1);
             } else {
+            	
             	//long start = System.nanoTime();
                 int    currVal = 0;   // current value
                 int currWei = 0; // current weight
                 for (int j = 0; j < n; j++) {
                 	// constraints
                 	// make it realistic, we can't sell more than 1 stacks quickly
-                	if(iIt[j] > 99*2) continue;
-                	if(iIt[j] > 10 && items[j].getPriority() == 3) continue;
-                	if(iIt[j] > 30 && items[j].getPriority() == 2) continue;
+                	if(iIt[j] > 99*2/3) continue;
+                	if(iIt[j] > 12/3 && items[j].getPriority() == 3) continue;
+                	//if(iIt[j] > 30/3 && items[j].getPriority() == 2) continue;
                 	
                     currVal += iIt[j] * items[j].getValue();
                     currWei += iIt[j] * items[j].getWeight();
@@ -121,8 +98,8 @@ public class UnboundedKnapsack2 {
                     best.setValue (currVal);
                     best.setWeight(currWei);
                     for (int j = 0; j < n; j++) bestAm[j] = iIt[j];
-                    System.out.println("Global: " + globalCount + "," + best.getValue());
-                } // if (...)
+                    System.out.println(best.getValue());
+                }
                 //System.out.println("base case time: " + (System.nanoTime() - start));
             } // else
         } // for (i)
@@ -131,7 +108,7 @@ public class UnboundedKnapsack2 {
     /*
      * Set up the array
      */
-	public void setUp(String file,int from, int to) throws IOException {
+	public void setUp(String file) throws IOException {
 		XSSFWorkbook wb = null;
 		try {
 			OPCPackage pkg = OPCPackage.open(new File(file));
@@ -139,39 +116,54 @@ public class UnboundedKnapsack2 {
 		    XSSFSheet sheet = wb.getSheetAt(0);
 		    XSSFRow row;
 		    XSSFCell cell;
+
+		    int rows; // No of rows
+		    rows = sheet.getPhysicalNumberOfRows();
 		    
-		    // get row
-		    int cols = sheet.getRow(1).getPhysicalNumberOfCells();
+		    int cols = 0; // No of columns
+		    int tmp = 0;
 		    
 		    // 1 less than rows since we ignored 1st row (row 0)
-		    this.items = new Item2[to-from+1];
-		    for(int r = from; r <= to; r++) {
+		    this.items = new Item2[rows-1];
+		  
+		    // always ignore first row
+		    
+		    // This trick ensures that we get the data properly even if it doesn't start from first few rows
+		    // Figure out the maximum # of cells (cols) that we will need to read table (since some rows will
+		    // have different number of cells, so we take the largest)
+		    for(int i = 1; i < 10 || i < rows; i++) {
+		        row = sheet.getRow(i);
+		        if(row != null) {
+		            tmp = row.getPhysicalNumberOfCells();
+		            if(tmp > cols) cols = tmp;
+		           
+		        }
+		    }
+		    for(int r = 1; r < rows; r++) {
 		        row = sheet.getRow(r);
 		        if(row != null) {
-		        	items[items.length-(1+(to-r))] = new Item2();
+		        	items[r-1] = new Item2();
 		            for(int c = 0; c < cols; c++) {
 		                cell = row.getCell((short)c);
 		                if(cell != null) {
 		                	// name
 		                	if(c == 0)
-		                		items[items.length-(1+(to-r))].setName(cell.getStringCellValue());
+		                		items[r-1].setName(cell.getStringCellValue());
 		                	else if(c==1)
-		                		items[items.length-(1+(to-r))].setWeight((int)cell.getNumericCellValue());
+		                		items[r-1].setWeight((int)cell.getNumericCellValue());
 		                	/*// price in market
 		                	else if(c==2)
-		                		items[rows-(1+(to-r))].setValue((int)cell.getNumericCellValue());*/
+		                		items[r-1].setValue((int)cell.getNumericCellValue());*/
 		                	/* Priority */
 		                	else if (c==3)
-		                		items[items.length-(1+(to-r))].setPriority((int)cell.getNumericCellValue());
+		                		items[r-1].setPriority((int)cell.getNumericCellValue());
 		                	/* Current resell price*/
 		                	else if (c==4)
-		                		items[items.length-(1+(to-r))].setValue((int)cell.getNumericCellValue());
-		                	else if (c > 4)
-		                		System.err.println("Error! Check excel file");
+		                		items[r-1].setValue((int)cell.getNumericCellValue());
+		                	/*else if (c > 4)
+		                		System.err.println("Error! Check excel file");*/
 		                }
 		            }
-		            System.out.println("Row: " + r + " " + items[items.length-(1+(to-r))].getName() + " " + 
-		            		items[items.length-(1+(to-r))].getWeight() + " " + items[items.length-(1+(to-r))].getValue());
 		        }
 		    }
 		} catch(Exception e) {
